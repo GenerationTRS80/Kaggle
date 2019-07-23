125dc736110c7c7760dc071dd12a5b92c9f860bd
Phil <gentrs80@gmail.com>
2019-07-22 20:36:18 -0700

    add: dataframe to return table data

--- Kaggle_Lessons/SQL_SummerCamp/Exercise_JoinData.py
+++ Kaggle_Lessons/SQL_SummerCamp/Exercise_JoinData.py
@@ -9,7 +9,27 @@ dataset_ref = client.dataset("stackoverflow", project="bigquery-public-data")
 # API request - fetch the dataset
 dataset = client.get_dataset(dataset_ref)
 
-tables = list(client.list_tables(dataset))
+# #Get list of tables
+# tables = list(client.list_tables(dataset))
 
-for table in tables:
-    print(table.table_id)
+# list_of_tables = [table.table_id for table in tables]
+# print(list_of_tables)
+
+# for table in tables:
+#     print(table.table_id)
+
+table_ref = dataset_ref.table('posts_answers')
+tblReturned = client.get_table(table_ref)
+
+# Print fields
+print(tblReturned.table_id)
+
+# tblReturned_Fields = [field.name for field in tblReturned.schema]
+# print(tblReturned_Fields)
+
+for field in tblReturned.schema:
+    print(field.name)
+
+df = client.list_rows(tblReturned, max_results=5).to_dataframe()
+
+print(df)
