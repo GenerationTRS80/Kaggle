f1c58187eaa7700372b2f7ae327ba11185f278e4
Phil <gentrs80@gmail.com>
2019-08-02 19:51:22 -0700

    init 2: Exercise List

--- Kaggle_Lessons/PythonCourse/.vscode/Exercise_Lists.py
+++ Kaggle_Lessons/PythonCourse/.vscode/Exercise_Lists.py
@@ -16,46 +16,46 @@ hands = [
 hands = [['J', 'Q', 'K'], ['2', '2', '2'], ['6', 'A', 'K']]
 
 
-# # mix of different types of variables:
-# my_favourite_things = [32, 'raindrops on roses', help]
-# print(my_favourite_things)
+# mix of different types of variables:
+my_favourite_things = [32, 'raindrops on roses', help]
+print(my_favourite_things)
 
 
-# print(planets[0])
-# print(planets[1])
-# print('Planets furthest from the sun', planets[-1])
-# print('First 3 planets', planets[0:3])
-# print('Last 5 planets', planets[3:])
-# print('Last 3 planets', planets[-3:])
+print(planets[0])
+print(planets[1])
+print('Planets furthest from the sun', planets[-1])
+print('First 3 planets', planets[0:3])
+print('Last 5 planets', planets[3:])
+print('Last 3 planets', planets[-3:])
 
-# # Negative indices
-# # All the planets except the first and last
-# print(planets[1:-1])
+# Negative indices
+# All the planets except the first and last
+print(planets[1:-1])
 
-# # modify a list is to assign to an index or slice expression.
-# planets[3] = 'Malacandra'
-# print('Rename Mars to Malacandra', planets)
+# modify a list is to assign to an index or slice expression.
+planets[3] = 'Malacandra'
+print('Rename Mars to Malacandra', planets)
 
 
-# planets[:3] = ['Mur', 'Vee', 'Ur']
-# print(planets)
-# # That was silly. Let's give them back their old names
-# planets[:4] = ['Mercury', 'Venus', 'Earth', 'Mars', ]
+planets[:3] = ['Mur', 'Vee', 'Ur']
+print(planets)
+# That was silly. Let's give them back their old names
+planets[:4] = ['Mercury', 'Venus', 'Earth', 'Mars', ]
 
-# # Use carraige return "\n\r"
-# print("Set List of planets"+'\n\r', planets)
+# Use carraige return "\n\r"
+print("Set List of planets"+'\n\r', planets)
 
-# # How many planets are there?
-# print(len(planets))
+# How many planets are there?
+print(len(planets))
 
-# # The planets sorted in alphabetical order
-# sort_planets = sorted(planets)
-# print('Sort planets A-Z', sort_planets)
+# The planets sorted in alphabetical order
+sort_planets = sorted(planets)
+print('Sort planets A-Z', sort_planets)
 
-# # sum
-# primes = [2, 3, 5, 7]
-# print('Total = ', sum(primes))
-# print('Max = ', max(primes))
+# sum
+primes = [2, 3, 5, 7]
+print('Total = ', sum(primes))
+print('Max = ', max(primes))
 
 # #Interlude: Objects
 # I've used the term 'object' a lot so far - you may have even read that everything in Python is an object. What does that mean?
